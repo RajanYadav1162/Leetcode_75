8c5eb1cd604f2a025d3200d48f99616084c40421
Rajan <rajanyadav1162@gmail.com>
2024-09-27 17:57:55 +0100

    add solution for can Place Flower

--- Array String/Can Place Flowers/Main.java
+++ Array String/Can Place Flowers/Main.java
@@ -1,38 +1,51 @@
+import java.util.*;
+
+/*
+You have a long flowerbed in which some of the plots are planted, and some are not. However,
+flowers cannot be planted in adjacent plots.
+
+Given an integer array flowerbed containing 0's and 1's, where 0 means empty and 1 means not empty,
+and an integer n, return true if n new flowers can be planted in the flowerbed without violating the no-adjacent-flowers
+rule and false otherwise.
+
+Example 1:
+
+Input: flowerbed = [1,0,0,0,1], n = 1
+Output: true
+Example 2:
+
+Input: flowerbed = [1,0,0,0,1], n = 2
+Output: false
+ */
+
+//solution-idea
+/*
+For each position 'i', we have to check
+  1. flowerbed[i] == 0 && flowerbed[i-1] == 0 && flowerbed[i+1] == 0.
+  2. it's better to create one extra array (n+2) size, that will make handling of edge case like for i==0 && i == n-1
+     bit easier.
+ */
 
 class Solution {
-   public boolean canPlaceFlowers(int[] A, int n) {
 
-      int mx = 0;
-      int len = A.length;
+  public boolean canPlaceFlowers(int[] flowerbed, int n) {
 
-      if (len == 0) {
-         return mx >= n;
+    int len = flowerbed.length;
+    int[] temp = new int[len + 2];
+
+    //if initially flowerbed=[1, 2, 3], temp will contains [0, 1, 2, 3, 0].
+    System.arraycopy(flowerbed, 0, temp, 1, len);
+
+    int count = 0;
+
+    //simple left and right checks, make sure too mark the values as '1', so that it can't be added as duplicate.
+    for (int i = 1; i <= len; i++) {
+      if (temp[i] == 0 && temp[i - 1] == 0 && temp[i + 1] == 0) {
+        count++;
+        temp[i] = 1;
       }
+    }
 
-      if (len == 1) {
-         mx = A[0] == 0 ? 1 : 0;
-         return mx >= n;
-      }
-
-      for (int i = 0; i < n; i++) {
-         if (i + 1 < len && i == 0 && A[i + 1] == 0) {
-            A[i] = 1;
-            mx++;
-         } else if (i > 0 && i + 1 < len && A[i] == 0 && A[i - 1] == 0 && A[i + 1] == 0) {
-            A[i] = 0;
-            mx++;
-         }
-      }
-
-      return mx >= n;
-
-   }
-}
-
-class Main {
-   public static void main(String[] args) {
-
-      var res = new Solution().canPlaceFlowers(new int[] { 1, 0, 0, 0, 0, 0, 1 }, 2);
-      System.out.println(res);
-   }
+    return count >= n;
+  }
 }
